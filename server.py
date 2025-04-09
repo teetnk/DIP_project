752eecf411f74abd0f2d1628e6470439dfa9e567
tunpiyachok <piyachok.r@ku.th>
2025-04-10 01:28:51 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
-/*
+"""
 Development by :
 Thanakorn Sutakiatsakul
 Piyachok Ridsadaeng
-*/
+"""
 from flask import Flask, render_template, request, jsonify, send_from_directory
 import os
 from flask_cors import CORS
